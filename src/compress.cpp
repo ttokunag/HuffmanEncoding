75dd18a3ddb7a18bf887b9159091c7d78d2812ac
ttokunag <ttokunag@ucsd.edu>
2020-02-12 00:37:18 +0000

    added cxxopt parts

--- src/compress.cpp
+++ src/compress.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <iostream>
 
+#include <cxxopts.hpp>
 #include "FileUtils.hpp"
 #include "HCNode.hpp"
 #include "HCTree.hpp"
@@ -18,4 +19,27 @@ void pseudoCompression(string inFileName, string outFileName) {}
 void trueCompression(string inFileName, string outFileName) {}
 
 /* TODO: Main program that runs the compress */
-int main(int argc, char* argv[]) { return 0; }
+int main(int argc, char* argv[]) {
+    cxxopts::Options options("./compress",
+                             "Compresses files using Huffman Encoding");
+    options.positional_help("./path_to_input_file ./path_to_output_file");
+
+    bool isAsciiOutput = false;
+    string inFileName, outFileName;
+    options.allow_unrecognised_options().add_options()(
+        "ascii", "Write output in ascii mode instead of bit stream",
+        cxxopts::value<bool>(isAsciiOutput))(
+        "input", "", cxxopts::value<string>(inFileName))(
+        "output", "", cxxopts::value<string>(outFileName))(
+        "h,help", "Print help and exit");
+
+    options.parse_positional({"input", "output"});
+    auto userOptions = options.parse(argc, argv);
+
+    if (userOptions.count("help") || !FileUtils::isValidFile(inFileName) ||
+        outFileName.empty()) {
+        cout << options.help({""}) << std::endl;
+    }
+
+    return 0;
+}
